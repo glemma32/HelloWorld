8b201d088bc2d810b152b1001325235f7c9230fc
glemma32 <genet.woldeyes@gmail.com>
2017-05-24 17:40:58 -0400

    First java program commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+
+public class HelloWorld {
+
+	/* This is a multi-line comment
+	 * Author: djw
+	 * Date: 5/24/17
+	 * this is our first program
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!");
+
+	}
+
+}
